8b09dc3122f59eb509bad3b5d5240abb79b93590
paezxavi <paezxavie@gmail.com>
2018-01-11 09:58:28 +0100

    Ajoute utilisation Pivot

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    
+  public function products()
+  {
+      return $this->belongsToMany('App\Product','customers_products_pivot','customer_id','product_id')->withPivot('quantity');
+  }
 }
--- app/Product.php
+++ app/Product.php
@@ -11,4 +11,9 @@ class Product extends Model
     protected $fillable = [
         'image', 'nom', 'categorie', 'prixVente', 'reference', 'refSupplier', 'prixAchat',
     ];
+
+    public function customers()
+    {
+        return $this->belongsToMany('App\Customer','customers_products_pivot','product_id','customer_id')->withPivot('quantity');
+    }
 }
