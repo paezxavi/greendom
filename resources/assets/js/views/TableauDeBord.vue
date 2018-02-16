db470a4479768e4ae62b4fedaf2665bacbaa5711
paezxavi <paezxavie@gmail.com>
2018-02-16 15:24:31 +0100

    Ajoute tableau + Model Status

---
+++ resources/assets/js/views/TableauDeBord.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="container">
+
+      <section>
+        <b-field grouped group-multiline>
+            <b-select v-model="defaultSortDirection">
+              <option value="asc">Default sort direction: ASC</option>
+              <option value="desc">Default sort direction: DESC</option>
+            </b-select>
+            <b-select v-model="perPage" :disabled="!isPaginated">
+              <option value="5">5 per page</option>
+              <option value="10">10 per page</option>
+              <option value="15">15 per page</option>
+              <option value="20">20 per page</option>
+            </b-select>
+            <div class="control">
+              <button class="button" @click="currentPage = 2" :disabled="!isPaginated">Set page to 2</button>
+            </div>
+            <div class="control is-flex">
+              <b-switch v-model="isPaginated">Paginated</b-switch>
+            </div>
+            <div class="control is-flex">
+              <b-switch v-model="isPaginationSimple" :disabled="!isPaginated">Simple pagination</b-switch>
+            </div>
+        </b-field>
+        <b-table
+            :data="arrayDevis"
+            :paginated="isPaginated"
+            :per-page="perPage"
+            :current-page.sync="currentPage"
+            :pagination-simple="isPaginationSimple"
+            :default-sort-direction="defaultSortDirection"
+            default-sort="dateDebut">
+
+            <template slot-scope="props">
+                <b-table-column field="dateDebut" label="Date début" sortable>
+                    {{ new Date(props.row.dateDebut).toLocaleDateString() }}
+                </b-table-column>
+
+                <b-table-column field="update_at" label="Dernière modification" sortable>
+                    {{ props.row.update_at }}
+                </b-table-column>
+
+                <b-table-column field="concerne" label="Concerne" sortable>
+                    {{ props.row.concerne }}
+                </b-table-column>
+
+                <b-table-column field="status.nom" label="Statut" sortable>
+                    {{ props.row.status.nom }}
+                </b-table-column>
+            </template>
+        </b-table>
+      </section>
+    </div>
+</template>
+
+
+<script>
+    import moment from 'moment';
+
+    export default {
+
+        //Problème avec l'icone de "sort", il faut trouver comment l'afficher correctement
+
+        data() {
+            return {
+                arrayDevis: [],
+                isPaginated: true,
+                isPaginationSimple: false,
+                defaultSortDirection: 'asc',
+                currentPage: 1,
+                perPage: 5
+            }
+        },
+
+        created() {
+            axios.get('/devis/'+this.$route.params.user)
+                .then(({data}) => this.arrayDevis = data);
+        },
+
+
+        methods: {
+          dateDebutDevis(devis){
+            return moment(devis.dateDebut).format('DD/MM/YYYY');
+          },
+        }
+    }
+</script>
