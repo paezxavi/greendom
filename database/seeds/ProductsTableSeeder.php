6a9fa8f8f2e2dd2dd3fe71f8e8d26c89654a68a9
Paez Rodriguez Luis Xavie <luisxavi.paezrodr@hes-so.ch>
2018-04-28 14:51:53 +0200

    Ajoute de nouvelles images pour la BDD products

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -23,7 +23,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '12'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'http://greendom.eu/shop/img/p/1/5/GD-E14-E12B-4W.jpg',
                 'nom' => "T2 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -33,7 +33,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'http://greendom.eu/shop/img/p/1/5/GD-E27-A60P.jpg',
                 'nom' => "T3 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -43,7 +43,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'http://www.decoreno.fr/Files/28053/Img/19/encastrable-rond-led-extra-plat.jpg',
                 'nom' => "T4 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -53,7 +53,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://i.hinnavaatlus.ee/p/900/ae/68/8cb25f046b9682deba8d182d30a19934a9cf58831_4261.jpg',
                 'nom' => "T5 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -63,7 +63,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://p1.eanfind.com/ph_img/first/P/2/B/Z/6N8EHKZB2P.jpg',
                 'nom' => "T6 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -73,7 +73,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://d33i8j38ri7gad.cloudfront.net/made-in-design/small/made-in-design_2caaae7aeec911e6bbc50261ac2dc7eb.jpg',
                 'nom' => "T7 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -83,7 +83,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://www.lightingstyles.co.uk/pics/1/White_Adjustable_Track_Head_Gold_Detailing.jpg',
                 'nom' => "T8 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -93,7 +93,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://www.ledexel.com/958-large_default/spot-led-cob-encastrable-7-watts-4000k.jpg',
                 'nom' => "T9 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -103,7 +103,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCNXJvoWFa-8mcvxcE_8Yr2ZHy73k0MWc19p1YEQ03VikVAiHkJA',
                 'nom' => "T10 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -113,7 +113,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'http://www.swiss-leds.ch/1493-home_default3/v-tac-led-spot-encastrable-downlight-10-watt-3000k-730lm-120-cadre-rond-blanc.jpg',
                 'nom' => "T11 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -123,7 +123,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://www.batiproduits.com/img/plafonnier-a-spots-led-orientables-2x8w-pour-eclairage-interieur-gyro-cube-2x8w-007690940-product_maxi.jpg',
                 'nom' => "T12 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -133,7 +133,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://themoc.fr/35-home_default/ampoule-led-couleur-smart-led-e27.jpg',
                 'nom' => "T13 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -143,7 +143,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://image.darty.com/telephonie/eclairage_domotique/ampoules_connectees/tp-link_lb120_p1801244389417A_152412960.jpg',
                 'nom' => "T14 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -153,7 +153,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://www.promoled.fr/10692-8029-large_default/spot-led-fixe-4-zones-ip44-13w-rgb-blanc-chaud.jpg',
                 'nom' => "T15 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -163,7 +163,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://www.france-xenon.com/812-1762-large_default/tube-neon-led-t8-120cm-verre-opaque-haut-de-gamme.jpg',
                 'nom' => "T16 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -173,7 +173,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://www.led-horticoles.eu/wp-content/uploads/2015/08/neon-led-90.jpg',
                 'nom' => "T17 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -183,7 +183,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://www.vipgros.com/1794-large_default/tube-neon-led-t8-single.jpg',
                 'nom' => "T18 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -193,7 +193,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://www.anrold-diffusion.com/Files/21518/Img/19/50432-photo-1-big.jpg',
                 'nom' => "T19 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
@@ -203,7 +203,7 @@ class ProductsTableSeeder extends Seeder
                 'prixAchat' => '15'
             ],
             [
-                'image' => 'http://greendom.eu/shop/img/p/1/5/SNT8B-XW.jpg',
+                'image' => 'https://www.deliled.com/583-large_default/ampoule-spot-led-gu10-35w-smd-ecolife.jpg',
                 'nom' => "T20 compatibles",
                 'categorie' => 'T8',
                 'description' => 'Ceci est une description pour décrire des ampoules',
